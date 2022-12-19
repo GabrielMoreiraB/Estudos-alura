0f3306d3ddc85c91ee72f6f959c9e108399b8110
Gabriel Barbosa Moreira <109036024+GabrielMoreiraB@users.noreply.github.com>
2022-12-19 20:53:56 -0300

    finish

--- html-css/1/index.js
+++ html-css/1/index.js
@@ -1,7 +1,9 @@
-function clickMenu(){
-    if (cabecalho__menu.style.display == 'flex'){
-        cabecalho__menu.style.display = none
-    }else {
-        cabecalho__menu.style.display = 'flex'
+var cabecalho__menu = document.querySelector(".cabecalho__menu");
+
+function clickMenu() {
+    if(cabecalho__menu.style.display == "flex"){
+        cabecalho__menu.style.display = "none"
+    } else {
+        cabecalho__menu.style.display = "flex";
     }
 }
